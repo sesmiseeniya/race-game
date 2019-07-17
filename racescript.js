54def4123ef345b48b87980d0aabe322eb3aaf11
sesmiseeniya <sesmiseeniya1997@gmail.com>
2019-07-18 00:55:00 +0530

    car race game script

--- racescript.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    var anim_id;
-
-    var container = $('#container');
-    var car = $('#car_1');
-    var car =
-}
---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+    var anim_id;
+
+    var container = $('#container');
+    var car_1 = $('#car_1');
+    var car_2 = $('#car_2');
+    var car_3 = $('#car_3');
+
+    var line1 = $('#line_1');
+    var line2
+}
