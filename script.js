b10a0e4e6af0718ad8c6a243b338b442c88fd224
sesmiseeniya <sesmiseeniya1997@gmail.com>
2019-07-18 01:07:28 +0530

    car race game script

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ $(function () {
     var car_2 = $('#car_2');
     var car_3 = $('#car_3');
 
-    var line1 = $('#line_1');
-    var line2
+    var line_1 = $('#line_1');
+    var line_2 = $('#line_2');
+    var line_3 = $('#line_3');
+
+    var restart_div = $('#restart_div');
+    var restart_btn = $('#restart');
+    var score = $('#score');
+
+    var container_left = parseInt(container.css('left'));
+    var container_width = parseInt(container.width());
+    var container_height = parseInt(container.height());
+    var car_width = parseInt(car.width());
+    var car_height = parseInt(car.height());
+
 }
