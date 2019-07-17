593af4761f095c1094af8b5706c9996455ebdf2d
sesmiseeniya <sesmiseeniya1997@gmail.com>
2019-07-18 02:12:22 +0530

    car race game script

--- script.js
+++ script.js
@@ -32,7 +32,31 @@ $(function () {
     var move_up = false;
     var move_down = false;
 
-    $(document).on('key')
+    $(document).on('keydown',function (e) {
+        if(game_over === false){
+            var key = e.keyCode;
+            if(key === 37 && move_left === false){
+                move_left = requestAnimationFrame(left);
+            }else if (key === 39 && move_right === false) {
+                move_right = requestAnimationFrame(right);
+            }else if (key === 38 && move_up ===false){
+                move_up = requestAnimationFrame(up);
+            }else if (key === 40 && move_down ===false){
+                move_down = requestAnimationFrame(down);
+            }
+
+        }
+
+    });
+
+    
+
+
+
+
+
+
+
 
 
 
